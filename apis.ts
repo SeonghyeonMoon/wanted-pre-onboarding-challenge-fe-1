213ad551c09c034501cc7c8af5c85d8e5e0680b7
문성현 <shine7329@gmail.com>
2022-12-30 21:09:07 +0900

    ✨Feature: todo api 추가

--- apis.ts
+++ apis.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosResponse } from 'axios';
 import type {
+  CreateTodoRequest,
   LoginRequest,
   LoginResponse,
   SignUpRequest,
   SignUpResponse,
+  TodoData,
+  UpdateTodoRequest,
 } from './types';
 
 const baseURL = 'http://localhost:8080';
@@ -20,8 +23,29 @@ const signUp = (
 ): Promise<AxiosResponse<SignUpResponse>> =>
   instance.post('/users/create', signUpRequest);
 
+const getTodos = (): Promise<AxiosResponse<TodoData[]>> =>
+  instance.get('/todos');
+
+const getTodoById = (id: string): Promise<AxiosResponse<TodoData>> =>
+  instance.get(`/todos/${id}`);
+
+const createTodo = (
+  createTodoRequest: CreateTodoRequest,
+): Promise<AxiosResponse<TodoData>> =>
+  instance.post('/todos', createTodoRequest);
+
+const updateTodo = (
+  id: string,
+  updateTodoRequest: UpdateTodoRequest,
+): Promise<AxiosResponse<TodoData>> =>
+  instance.put(`/todos/${id}`, updateTodoRequest);
+
+const deleteTodo = (id: string): Promise<AxiosResponse> =>
+  instance.delete(`/todos/${id}`);
+
 const apis = {
   auth: { login, signUp },
+  todos: { getTodos, getTodoById, createTodo, updateTodo, deleteTodo },
 };
 
 export default apis;
--- types.ts
+++ types.ts
@@ -17,3 +17,21 @@ export type SignUpResponse = {
   message: string;
   token: string;
 };
+
+export type TodoData = {
+  title: string;
+  content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type CreateTodoRequest = {
+  title: string;
+  content: string;
+};
+
+export type UpdateTodoRequest = {
+  title: string;
+  content: string;
+};
