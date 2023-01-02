9f7c851251074502450a7fb192c0108b1f0a7a41
문성현 <shine7329@gmail.com>
2023-01-02 12:04:17 +0900

    ✨Feature: updateForm

--- components/todo/Item.tsx
+++ components/todo/Item.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import apis from '../../apis';
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
 
 type ItemProps = {
   id: string;
@@ -25,12 +26,84 @@ const Item = ({ id, title, content, createdAt, updatedAt }: ItemProps) => {
     deleteTodo();
   };
 
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [updateTodoFormData, setUpdateTodoFormData] = useState({
+    title,
+    content,
+  });
+
+  const { mutate: updateTodo } = useMutation(
+    () =>
+      apis.todos
+        .updateTodo(id, updateTodoFormData)
+        .then((res) => res.data.data),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['todos']);
+      },
+    },
+  );
+
+  const handleSubmitUpdateForm: FormEventHandler<HTMLFormElement> = async (
+    e,
+  ) => {
+    e.preventDefault();
+    updateTodo();
+    setIsUpdating(false);
+  };
+
+  const handleChangeUpdateForm: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { id, value } = e.target;
+    setUpdateTodoFormData((prev) => ({ ...prev, [id]: value }));
+  };
+
+  const isValidUpdateTodoFormData = () => {
+    const { title, content } = updateTodoFormData;
+    return title && content;
+  };
+
+  if (isUpdating) {
+    return (
+      <form onSubmit={handleSubmitUpdateForm}>
+        <label htmlFor='title'>Title</label>
+        <input
+          type='text'
+          id='title'
+          value={updateTodoFormData.title}
+          onChange={handleChangeUpdateForm}
+        />
+        <label htmlFor='content'>Content</label>
+        <input
+          type='text'
+          id='content'
+          value={updateTodoFormData.content}
+          onChange={handleChangeUpdateForm}
+        />
+        <button type='submit' disabled={!isValidUpdateTodoFormData()}>
+          Submit
+        </button>
+        <button
+          type='button'
+          onClick={() => {
+            setUpdateTodoFormData({ title, content });
+            setIsUpdating(false);
+          }}
+        >
+          Cancel
+        </button>
+      </form>
+    );
+  }
+
   return (
     <li>
       <p>Title: {title}</p>
       <p>Content: {content}</p>
       <p>Created At: {createdAt}</p>
       <p>Updated At: {updatedAt}</p>
+      <button type='button' onClick={() => setIsUpdating(true)}>
+        Update
+      </button>
       <button onClick={handleClickDeleteButton}>Delete</button>
     </li>
   );
