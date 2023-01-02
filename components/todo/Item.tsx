599d1cfce09dfa71aff4b03bd1781560e3a64dab
문성현 <shine7329@gmail.com>
2023-01-02 10:43:26 +0900

    ✨Feature: Todo Item 삭제

--- components/todo/Item.tsx
+++ components/todo/Item.tsx
@@ -1,3 +1,6 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import apis from '../../apis';
+
 type ItemProps = {
   id: string;
   title: string;
@@ -7,12 +10,28 @@ type ItemProps = {
 };
 
 const Item = ({ id, title, content, createdAt, updatedAt }: ItemProps) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteTodo } = useMutation(
+    () => apis.todos.deleteTodo(id).then((res) => res.data.data),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['todos']);
+      },
+    },
+  );
+
+  const handleClickDeleteButton = () => {
+    deleteTodo();
+  };
+
   return (
     <li>
       <p>Title: {title}</p>
       <p>Content: {content}</p>
       <p>Created At: {createdAt}</p>
       <p>Updated At: {updatedAt}</p>
+      <button onClick={handleClickDeleteButton}>Delete</button>
     </li>
   );
 };
