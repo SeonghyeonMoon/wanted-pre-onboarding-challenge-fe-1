0044944fea5b005454c7fdb41204ab369cc45428
문성현 <shine7329@gmail.com>
2023-01-02 10:02:13 +0900

    ✨Feature: 토큰 유무 확인, 기본 헤더 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { instance } from '../apis';
+import { List } from '../components/todo';
 
 const Todo = () => {
   const router = useRouter();
@@ -7,6 +10,15 @@ const Todo = () => {
     localStorage.removeItem('token');
     router.push('/auth');
   };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      instance.defaults.headers['Authorization'] = token;
+    } else {
+      router.push('/auth');
+    }
+  }, []);
 
   return (
     <>
